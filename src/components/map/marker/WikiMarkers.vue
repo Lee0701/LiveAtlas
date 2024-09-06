8da33d88bd8f9d3a8cf6d3c681496ba43f9e539d
Lee0701 <hgl1002@naver.com>
2024-09-07 02:02:35 +0900

    Update

--- src/components/map/marker/WikiMarkers.vue
+++ src/components/map/marker/WikiMarkers.vue
@@ -26,6 +26,8 @@ import {
 import {Layer} from "leaflet";
 import axios from "axios";
 
+const {apiEndpoint, categoryName} = window;
+
 export default defineComponent({
 	props: {
 		layerGroup: {
@@ -46,10 +48,10 @@ export default defineComponent({
 				action: 'query',
 				format: 'json',
 				list: 'categorymembers',
-				cmtitle: 'Category:지도 마커',
+				cmtitle: categoryName,
 				cmcontinue,
 			}
-			const resp = await axios.get("https://wiki.kubecity.kr/api.php", {params})
+			const resp = await axios.get(apiEndpoint, {params})
 			const {query} = resp.data;
 			const result = query.categorymembers;
 			if(!resp.data['continue']) return result;
@@ -63,41 +65,44 @@ export default defineComponent({
 				format: 'json',
 				page,
 			}
-			const resp = await axios.get("https://wiki.kubecity.kr/api.php", {params})
+			const resp = await axios.get(apiEndpoint, {params})
 			const {parse} = resp.data;
 			return parse.text['*'];
 		}
 
-		const extractMarkerTag = (text: string) => {
+		const extractMarkerTags = (text: string) => {
 			const div = document.createElement('div');
 			div.innerHTML = text;
-			const markerTag = div.querySelector('#map-marker');
-			const data = JSON.parse(markerTag.innerText);
-			return data;
+			const markerTags = div.querySelectorAll('.map-marker');
+			return [...markerTags].map((markerTag) => {
+				return JSON.parse(markerTag.innerText);
+			})
 		}
 
-		const createMarker = (title, data) => {
-			const {x, z, world, minzoom, maxzoom, icon} = data;
+		const createMarker = (data: Object) => {
+			const {name, x, z, world, minzoom, maxzoom, icon} = data;
 			const layer = createMarkerLayer({
-				id: title,
+				id: name,
 				type: LiveAtlasMarkerType.POINT,
 				location: {x, y: 64, z},
-				minZoom: minzoom,
-				maxZoom: maxzoom,
-				tooltip: title,
+				// minZoom: minzoom,
+				// maxZoom: maxzoom,
+				tooltip: name,
 				iconUrl: icon,
 				iconSize: [16, 16],
 			} as LiveAtlasPointMarker, converter)
-			layers.set(title, layer);
+			layers.set(name, layer);
 			props.layerGroup.addLayer(layer);
 		}
 
 		const createMarkers = () => {			
 			categoryMembers().then(async (members) => {
-				const markers = await Promise.all(members.map(async (member) => {
+				await Promise.all(members.map(async (member) => {
 					const text = await parse(member.title);
-					const data = extractMarkerTag(text);
-					createMarker(member.title, data);
+					extractMarkerTags(text).forEach((data) => {
+						data.title = member.title;
+						createMarker(data);
+					})
 				}));
 			})
 		};
